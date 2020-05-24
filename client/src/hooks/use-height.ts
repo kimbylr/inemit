cfc1e90b41b38fb99ed6c05e792733854e214bf8
jimynu <kim@bylr.ch>
2020-05-24 17:54:17 +0200

    add visualViewport type defs + refactor useHeight

--- client/src/hooks/use-height.ts
+++ client/src/hooks/use-height.ts
@@ -1,25 +1,17 @@
 import { useState, useLayoutEffect } from 'react';
 
-const hasVisualViewport = !!(window as any).visualViewport;
-
 export const useHeight = () => {
   const [height, setHeight] = useState(
-    (window as any).visualViewport?.height || window.innerHeight,
+    window.visualViewport?.height ?? window.innerHeight,
   );
 
   useLayoutEffect(() => {
     const update = () =>
-      setHeight((window as any).visualViewport?.height || window.innerHeight);
+      setHeight(window.visualViewport?.height ?? window.innerHeight);
 
-    hasVisualViewport
-      ? (window as any).visualViewport.addEventListener('resize', update)
-      : window.addEventListener('resize', update);
-
-    return () => {
-      hasVisualViewport
-        ? (window as any).visualViewport.removeEventListener('resize', update)
-        : window.removeEventListener('resize', update);
-    };
+    const viewport = window.visualViewport ?? window;
+    viewport.addEventListener('resize', update);
+    return () => viewport.removeEventListener('resize', update);
   }, []);
 
   return height;
