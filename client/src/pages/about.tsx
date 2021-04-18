f6c05c12e0eb7b89de122d681326b3c770b6f76a
Kim <kim@bylr.ch>
2021-04-18 17:22:26 +0200

    fix: Brush up about page

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -8,45 +8,44 @@ import { PageLayout } from '../layout/page-layout';
 export const About: FC = () => (
   <PageLayout>
     <Heading>Die Idee: inemit statt Bullshit.</Heading>
-    <ExplainerParagraph>
-      Inemit kombiniert{' '}
-      <Highlight color="yellow">wissenschaftliche Erkenntnisse</Highlight> und{' '}
-      <Highlight color="orange">Technologie</Highlight>, um{' '}
-      <Highlight color="green">effizientes und sinnvolles Lernen</Highlight> so einfach
-      wie möglich zu machen.
+    <ExplainerParagraph big>
+      Heisst: Die <Highlight color="yellow">beste Methode</Highlight> für{' '}
+      <Highlight color="green">effizientes Lernen</Highlight> als{' '}
+      <Highlight color="orange">moderne App</Highlight>.
     </ExplainerParagraph>
     <ExplainerParagraph>
-      Am Anfang steht die Erkenntnis, dass{' '}
+      Am Anfang steht die wissenschaftlich abgestützte Erkenntnis, dass{' '}
       <Highlight color="yellow">Wiederholung</Highlight> zentral ist, um Vokabeln zu
       lernen. Diesen Effekt machen sich analoge Lernkarteien schon seit langer Zeit
-      zunutze. Davon leitet sich die Methode der{' '}
+      zunutze.{' '}
       <ExtLink href="https://de.wikipedia.org/wiki/Spaced_repetition">
-        spaced repetition
+        Spaced repetition
       </ExtLink>{' '}
-      ab.
+      ist die etwas raffiniertere Version davon.
     </ExplainerParagraph>
     <ExplainerParagraph>
-      Digitales Lernen ermöglicht{' '}
-      <Highlight color="orange">systematisches Lernen</Highlight>: Jeder Lerninhalt hat
-      einen aus positiven und negativen Antworten berechneten{' '}
-      <Highlight color="orange">Schwierigkeitsgrad</Highlight>. Darauf basierend wird der{' '}
+      Eine App nicht deswegen, weil digital und ergo toll, sondern weil digital{' '}
+      <Highlight color="orange">systematisches Lernen</Highlight> erst ermöglicht: Die App
+      kümmert sich darum, dass Vokabeln desto seltener angezeigt werden, je besser du sie
+      kannst. Aus jeder richtigen oder falschen Antwort wird der Schwierigkeitsgrad neu
+      berechnet. Darauf basierend wird der{' '}
       <Highlight color="orange">optimale Zeitabstand</Highlight> bis zur nächsten Abfrage
-      gesetzt. So kann die Lernzeit effizient genutzt werden. (<Inemit>Inemit</Inemit>{' '}
-      implementiert den{' '}
+      gesetzt. Das garantiert, dass die Lernzeit effizient genutzt wird. (
+      <Inemit>Inemit</Inemit> implementiert den{' '}
       <ExtLink href="https://supermemo.guru/wiki/SuperMemo_1.0_for_DOS_(1987)#Algorithm_SM-2">
         <em>SM-2</em>-Algorithmus
       </ExtLink>
       ).
     </ExplainerParagraph>
     <ExplainerParagraph>
-      Lernerfolg hängt von der eigenen Investition ab. Keine App der Welt kann einem das
-      Lernen abnehmen. Technologie kann nur unterstützen. Entscheidend sind neben
-      regelmässiger Wiederholung insbesondere{' '}
+      Inemit stellt die Basis für <Highlight color="green">sinnvolles Lernen</Highlight>{' '}
+      bereit – lernen musst du selbst. Technologie kann nur unterstützen. Entscheidend
+      sind neben regelmässiger Wiederholung insbesondere{' '}
       <Highlight color="green">passende Lerninhalte</Highlight> (vgl.{' '}
       <ExtLink href="https://universeofmemory.com/spaced-repetition-apps-dont-work/">
         Why Most Spaced Repetition Apps Don't Work
       </ExtLink>
-      ). Inemit soll zu <Highlight color="green">sinnvollem Lernen animieren</Highlight>.
+      ).
     </ExplainerParagraph>
 
     <TechnicalNotes>
@@ -59,8 +58,8 @@ export const About: FC = () => (
   </PageLayout>
 );
 
-const ExplainerParagraph = styled(Paragraph)`
-  font-size: ${({ theme: { font } }) => font.sizes.sm};
+const ExplainerParagraph = styled(Paragraph)<{ big?: boolean }>`
+  font-size: ${({ big, theme: { font } }) => font.sizes[big ? 'md' : 'sm']};
 `;
 
 const Highlight = styled.strong<{ color: 'orange' | 'yellow' | 'green' }>`
