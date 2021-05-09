3af1a33b70d385e35333be4063cdca41161f7d10
Kim <kim@bylr.ch>
2021-05-09 22:21:27 +0200

    fix: About page wording

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,61 +1,76 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
 import { version } from '../../../package.json';
+import { Header } from '../components/header';
+import { Main } from '../components/main';
+import { Menu } from '../components/menu';
 import { ExtLink } from '../elements/link';
 import { Heading, Paragraph } from '../elements/typography';
 import { PageLayout } from '../layout/page-layout';
 
 export const About: FC = () => (
-  <PageLayout>
-    <Heading>Die Idee: inemit statt Bullshit.</Heading>
-    <ExplainerParagraph big>
-      Heisst: Die <Highlight color="yellow">beste Methode</Highlight> für{' '}
-      <Highlight color="green">effizientes Lernen</Highlight> als{' '}
-      <Highlight color="orange">moderne App</Highlight>.
-    </ExplainerParagraph>
-    <ExplainerParagraph>
-      Am Anfang steht die wissenschaftlich abgestützte Erkenntnis, dass{' '}
-      <Highlight color="yellow">Wiederholung</Highlight> zentral ist, um Vokabeln zu
-      lernen. Diesen Effekt machen sich analoge Lernkarteien schon seit langer Zeit
-      zunutze.{' '}
-      <ExtLink href="https://de.wikipedia.org/wiki/Spaced_repetition">
-        Spaced repetition
-      </ExtLink>{' '}
-      ist die etwas raffiniertere Version davon.
-    </ExplainerParagraph>
-    <ExplainerParagraph>
-      Eine App nicht deswegen, weil digital und ergo toll, sondern weil digital{' '}
-      <Highlight color="orange">systematisches Lernen</Highlight> erst ermöglicht: Die App
-      kümmert sich darum, dass Vokabeln desto seltener angezeigt werden, je besser du sie
-      kannst. Aus jeder richtigen oder falschen Antwort wird der Schwierigkeitsgrad neu
-      berechnet. Darauf basierend wird der{' '}
-      <Highlight color="orange">optimale Zeitabstand</Highlight> bis zur nächsten Abfrage
-      gesetzt. Das garantiert, dass die Lernzeit effizient genutzt wird. (
-      <Inemit>Inemit</Inemit> implementiert den{' '}
-      <ExtLink href="https://supermemo.guru/wiki/SuperMemo_1.0_for_DOS_(1987)#Algorithm_SM-2">
-        <em>SM-2</em>-Algorithmus
-      </ExtLink>
-      ).
-    </ExplainerParagraph>
-    <ExplainerParagraph>
-      Inemit stellt die Basis für <Highlight color="green">sinnvolles Lernen</Highlight>{' '}
-      bereit – lernen musst du selbst. Technologie kann nur unterstützen. Entscheidend
-      sind neben regelmässiger Wiederholung insbesondere{' '}
-      <Highlight color="green">passende Lerninhalte</Highlight> (vgl.{' '}
-      <ExtLink href="https://universeofmemory.com/spaced-repetition-apps-dont-work/">
-        Why Most Spaced Repetition Apps Don't Work
-      </ExtLink>
-      ).
-    </ExplainerParagraph>
+  <>
+    <Header />
+    <Menu />
+    <Main>
+      <PageLayout>
+        <Heading>Die Idee: inemit statt Bullshit.</Heading>
+        <ExplainerParagraph big>
+          Die <Highlight color="yellow">beste Methode</Highlight> für{' '}
+          <Highlight color="green">effizientes Lernen</Highlight> als{' '}
+          <Highlight color="orange">moderne App</Highlight>.
+        </ExplainerParagraph>
+        <ExplainerParagraph>
+          <Highlight color="yellow">Mit Methode:</Highlight> Am Anfang steht die
+          wissenschaftlich abgestützte Erkenntnis, dass{' '}
+          <Highlight color="yellow">Wiederholung</Highlight> zentral ist, um Vokabeln zu
+          lernen. Diesen Effekt machen sich analoge Lernkarteien schon seit langer Zeit
+          zunutze.{' '}
+          <ExtLink href="https://de.wikipedia.org/wiki/Spaced_repetition">
+            Spaced repetition
+          </ExtLink>{' '}
+          ist die raffiniertere Version davon.
+        </ExplainerParagraph>
+        <ExplainerParagraph>
+          <Highlight color="orange">Eine App</Highlight> nicht deswegen, weil digital so
+          toll klingt, sondern weil es die ideale Voraussetzung für{' '}
+          <Highlight color="orange">systematisches Lernen</Highlight> ist: Die App kümmert
+          sich darum, dass gut verankerte Vokabeln seltener angezeigt werden. Der
+          Schwierigkeitsgrad wird bei jeder richtigen oder falschen Antwort neu berechnet.
+          Daraus ergibt sich der{' '}
+          <Highlight color="orange">optimale Zeitabstand</Highlight> bis zur nächsten
+          Abfrage. So ist garantiert, dass die Lernzeit effizient genutzt wird. (
+          <Inemit>Inemit</Inemit> implementiert den{' '}
+          <ExtLink href="https://supermemo.guru/wiki/SuperMemo_1.0_for_DOS_(1987)#Algorithm_SM-2">
+            <em>SM-2</em>-Algorithmus
+          </ExtLink>
+          ).
+        </ExplainerParagraph>
+        <ExplainerParagraph>
+          Inemit stellt die Basis für{' '}
+          <Highlight color="green">sinnvolles Lernen</Highlight> bereit – lernen musst du
+          selbst. Technologie kann nur unterstützen. Entscheidend sind neben regelmässiger
+          Wiederholung insbesondere{' '}
+          <Highlight color="green">passende Lerninhalte</Highlight> (vgl.{' '}
+          <ExtLink href="https://universeofmemory.com/spaced-repetition-apps-dont-work/">
+            Why Most Spaced Repetition Apps Don't Work
+          </ExtLink>
+          ).
+        </ExplainerParagraph>
 
-    <TechnicalNotes>
-      Version: {version} •{' '}
-      <ExtLink href="https://github.com/kimbylr/inemit/blob/master/CHANGELOG.md">
-        Changelog
-      </ExtLink>{' '}
-      • <ExtLink href="https://github.com/kimbylr/inemit/">Quellcode auf Github</ExtLink>
-    </TechnicalNotes>
-  </PageLayout>
+        <TechnicalNotes>
+          Version: {version} •{' '}
+          <ExtLink href="https://github.com/kimbylr/inemit/blob/master/CHANGELOG.md">
+            Changelog
+          </ExtLink>{' '}
+          •{' '}
+          <ExtLink href="https://github.com/kimbylr/inemit/">
+            Quellcode auf Github
+          </ExtLink>
+        </TechnicalNotes>
+      </PageLayout>
+    </Main>
+  </>
 );
 
 const ExplainerParagraph = styled(Paragraph)<{ big?: boolean }>`
