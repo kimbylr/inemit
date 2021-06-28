7f6249d5b40872ea9f489e6621b90ee6ee00d289
Kim <kim@bylr.ch>
2021-06-28 21:26:14 +0200

    fix: improve about page

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -21,37 +21,34 @@ export const About: FC = () => (
           <Highlight color="orange">moderne App</Highlight>.
         </ExplainerParagraph>
         <ExplainerParagraph>
-          <Highlight color="yellow">Mit Methode:</Highlight> Am Anfang steht die
-          wissenschaftlich abgestützte Erkenntnis, dass{' '}
-          <Highlight color="yellow">Wiederholung</Highlight> zentral ist, um Vokabeln zu
-          lernen. Diesen Effekt machen sich analoge Lernkarteien schon seit langer Zeit
-          zunutze.{' '}
+          <Highlight color="yellow">Yay science!</Highlight> Am Anfang steht die
+          Erkenntnis, dass <Highlight color="yellow">Wiederholung</Highlight> zentral ist,
+          um Vokabeln zu lernen – ein Effekt, den sich analoge Lernkarteien schon seit
+          langer Zeit zunutze machen.{' '}
           <ExtLink href="https://de.wikipedia.org/wiki/Spaced_repetition">
             Spaced repetition
           </ExtLink>{' '}
-          ist die raffiniertere Version davon.
+          ist die raffinierte Version davon.
         </ExplainerParagraph>
         <ExplainerParagraph>
-          <Highlight color="orange">Eine App</Highlight> nicht deswegen, weil digital so
-          toll klingt, sondern weil es die ideale Voraussetzung für{' '}
-          <Highlight color="orange">systematisches Lernen</Highlight> ist: Die App kümmert
-          sich darum, dass gut verankerte Vokabeln seltener angezeigt werden. Der
-          Schwierigkeitsgrad wird bei jeder richtigen oder falschen Antwort neu berechnet.
-          Daraus ergibt sich der{' '}
-          <Highlight color="orange">optimale Zeitabstand</Highlight> bis zur nächsten
-          Abfrage. So ist garantiert, dass die Lernzeit effizient genutzt wird. (
-          <Inemit>Inemit</Inemit> implementiert den{' '}
+          <Highlight color="orange">Eine App</Highlight> nicht deswegen, weil digital =
+          toll, sondern weil digital = ideale Voraussetzung für{' '}
+          <Highlight color="orange">systematisches Lernen</Highlight>. Die App kümmert
+          sich darum, dass gut verankerte Vokabeln seltener abgefragt werden – und falsch
+          beantwortete häufiger. Der{' '}
           <ExtLink href="https://supermemo.guru/wiki/SuperMemo_1.0_for_DOS_(1987)#Algorithm_SM-2">
             <em>SM-2</em>-Algorithmus
           </ExtLink>
-          ).
+          , den <Inemit>Inemit</Inemit> implementiert, versucht den{' '}
+          <Highlight color="orange">optimalen Zeitabstand</Highlight> bis zur nächsten
+          Abfrage zu setzen. So ist garantiert, dass die Lernzeit effizient genutzt wird.
         </ExplainerParagraph>
         <ExplainerParagraph>
-          Inemit stellt die Basis für{' '}
+          Technologie kann nur unterstützen. Inemit stellt die Basis für{' '}
           <Highlight color="green">sinnvolles Lernen</Highlight> bereit – lernen musst du
-          selbst. Technologie kann nur unterstützen. Entscheidend sind neben regelmässiger
-          Wiederholung insbesondere{' '}
-          <Highlight color="green">passende Lerninhalte</Highlight> (vgl.{' '}
+          selbst. Und die Lerninhalte machst du am besten auch selbst. Denn neben
+          regelmässiger Wiederholung sind{' '}
+          <Highlight color="green">passende Lerninhalte</Highlight> entscheidend (vgl.{' '}
           <ExtLink href="https://universeofmemory.com/spaced-repetition-apps-dont-work/">
             Why Most Spaced Repetition Apps Don't Work
           </ExtLink>
