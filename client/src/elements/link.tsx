79f5b2000f2656bff513684bc2f0d3a0fc1a5946
Kim <kim@bylr.ch>
2023-06-12 15:34:59 +0200

    fix: Link component typing and props passing

--- client/src/elements/link.tsx
+++ client/src/elements/link.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react';
+import { AnchorHTMLAttributes, FC } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-export const Link: FC<any> = (props) => (
-  <RouterLink className="text-primary-100 underline hover:text-primary-150" {...props} />
+export const Link: FC<any> = ({ className, ...props }) => (
+  <RouterLink
+    className={`text-primary-100 underline hover:text-primary-150 ${className}`}
+    {...props}
+  />
 );
 
-export const ExtLink: FC<any> = (props) => (
+export const ExtLink: FC<AnchorHTMLAttributes<HTMLAnchorElement>> = (props) => (
   <a className="text-primary-100 underline hover:text-primary-150" {...props} />
 );
