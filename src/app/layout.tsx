470a7c0fc9857604206ad401bd1de6f1acee9e0d
Kim <kim@bylr.ch>
2023-11-19 22:06:41 +0100

    fix: Use notch area in iOS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => (
   <UserProvider>
     <Head>
       <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
       <link
         rel="apple-touch-startup-image"
         href="/splashscreens/apple-splash-2048-2732.jpg"
