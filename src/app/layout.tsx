92a5365f343ff34bf4aafc7bbc13989d8e745c29
Kim <kim@bylr.ch>
2023-11-19 22:17:56 +0100

    fix: Add meta tags to use notch area

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,8 @@ const RootLayout: FC<{ children: ReactNode }> = ({ children }) => (
     <Head>
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      <meta name="viewport" content="width=device-width; initial-scale=1; viewport-fit=cover" />
+
       <link
         rel="apple-touch-startup-image"
         href="/splashscreens/apple-splash-2048-2732.jpg"
