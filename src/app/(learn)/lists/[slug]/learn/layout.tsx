f01efe972b1c5258a9da3217da608bf02df5cc6f
Kim <21242860+kimbylr@users.noreply.github.com>
2024-01-15 09:56:36 +0100

    fix: Keep body background color green in learn mode

--- src/app/(learn)/lists/[slug]/learn/layout.tsx
+++ src/app/(learn)/lists/[slug]/learn/layout.tsx
@@ -6,11 +6,6 @@ import { FC, ReactNode, useLayoutEffect } from 'react';
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
   const height = useHeight();
 
-  useLayoutEffect(() => {
-    document.getElementsByTagName('body')[0].style.backgroundColor = '#000';
-    () => document.getElementsByTagName('body')[0].style.removeProperty('background-color');
-  }, []);
-
   return (
     <div
       className="w-[100vw] overflow-hidden fixed bg-grey-98 p-4"
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,27 +6,20 @@ import { List } from '@/types/types';
 import Link from 'next/link';
 import { FC, useLayoutEffect } from 'react';
 
-export const Header: FC<{ lists: List[] }> = ({ lists }) => {
-  useLayoutEffect(() => {
-    const body = document.getElementsByTagName('body')[0];
-    body.style.backgroundColor = '#6CC17A';
-  }, []);
-
-  return (
-    <header
-      className="w-full bg-gradient-to-b from-primary-100 to-primary-150 select-none flex justify-center shadow-[0_5px_15px_#00000066]"
-      style={{ padding: 'max(env(safe-area-inset-top), 0.5rem) 0 0.5rem' }}
+export const Header: FC<{ lists: List[] }> = ({ lists }) => (
+  <header
+    className="w-full bg-gradient-to-b from-primary-100 to-primary-150 select-none flex justify-center shadow-[0_5px_15px_#00000066]"
+    style={{ padding: 'max(env(safe-area-inset-top), 0.5rem) 0 0.5rem' }}
+  >
+    <Link
+      href="/"
+      className="text-grey-98 leading-none my-1 flex justify-center dotted-focus dotted-focus-white"
     >
-      <Link
-        href="/"
-        className="text-grey-98 leading-none my-1 flex justify-center dotted-focus dotted-focus-white"
-      >
-        <IconLogo className="w-10" />
-        <span className="font-massive text-xxl ml-4">inemit!</span>
-      </Link>
-      <div className="absolute top-4 right-4 z-40">
-        <Menu lists={lists} />
-      </div>
-    </header>
-  );
-};
+      <IconLogo className="w-10" />
+      <span className="font-massive text-xxl ml-4">inemit!</span>
+    </Link>
+    <div className="absolute top-4 right-4 z-40">
+      <Menu lists={lists} />
+    </div>
+  </header>
+);
