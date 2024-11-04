ecfe884edaf9110d205c044f1979cdcdc30ed19a
Kim <21242860+kimbylr@users.noreply.github.com>
2024-11-04 21:48:31 +0100

    fix: Undo redo text with breaks

--- src/app/(learn)/lists/[slug]/learn/text-with-breaks.tsx
+++ src/app/(learn)/lists/[slug]/learn/text-with-breaks.tsx
@@ -1,16 +1,22 @@
-import { FC } from 'react';
+import { classNames } from '@/helpers/class-names';
+import React, { Fragment, FC } from 'react';
 
 // Add line breaks for long text with commas (2+ separate meanings)
 export const TextWithBreaks: FC<{ children: string }> = ({ children }) => {
-  const parts = children.split(/(,;) /);
+  const parts = children.split(', ');
 
-  if (parts.every((part) => part.length < 10)) {
+  if (parts.every((part) => part.length < 12)) {
     return children;
   }
 
-  return (
-    <span className="whitespace-pre">
-      {children.replaceAll(', ', ',\n').replaceAll('; ', ';\n')}
-    </span>
-  );
+  return parts.flatMap((part, i) => (
+    <Fragment key={i}>
+      {i !== 0 && (
+        <>
+          ,<br />
+        </>
+      )}
+      {part}
+    </Fragment>
+  ));
 };
