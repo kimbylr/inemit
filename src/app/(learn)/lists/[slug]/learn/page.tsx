bb8eed029f3d2a754a5dc2d05d19422b12b96c9a
Kim <21242860+kimbylr@users.noreply.github.com>
2025-12-23 21:42:31 +0100

    fix: List slug in SSR

--- src/app/(learn)/lists/[slug]/learn/page.tsx
+++ src/app/(learn)/lists/[slug]/learn/page.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 const LearnPage: FC<Props> = async ({ params }) => {
-  const slug = await params;
+  const slug = (await params)?.slug;
   const list = typeof slug === 'string' && (await getListToLearn(slug));
 
   if (!list) {
--- src/app/lists/[slug]/edit/page.tsx
+++ src/app/lists/[slug]/edit/page.tsx
@@ -8,7 +8,7 @@ type Props = {
 };
 
 const EditListPage: FC<Props> = async ({ params }) => {
-  const slug = await params;
+  const slug = (await params)?.slug;
   const list = typeof slug === 'string' && (await getListToEdit(slug));
 
   if (!list) {
